ea413bd5ca31721d578249104177426f996b8455
Maryna Khudorozhkova <kenimaaa@gmail.com>
2022-06-01 14:59:03 +0300

    Finish first task

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,31 @@
 // Add imports above this line
-import * as galleryItems  from './gallery-items';
-// Change code below this line
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
 
-console.log(galleryItems);
+import { galleryItems } from './gallery-items';
+// Change code below this line
+console.dir(galleryItems);
+
+const blockGallery = document.querySelector('.gallery');
+
+const galleryList = galleryItems.map(item =>
+  `<div class="gallery__item">
+  <a class="gallery__link" href="${item.original}">
+    <img
+      class="gallery__image"
+      src="${item.preview}"
+      data-source="${item.original}"
+      alt="${item.description}"
+    />
+  </a>
+</div>`
+).join('');
+
+blockGallery.innerHTML = galleryList;
+
+const lightbox = new SimpleLightbox('.gallery a', {
+  /* options */
+  captionsData: 'alt',
+  captionDelay: 250,
+});
+console.log(lightbox);
