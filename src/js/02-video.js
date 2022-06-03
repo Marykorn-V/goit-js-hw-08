8ad2304fa68f0d28654eb7fe90f62db256ccc364
Maryna Khudorozhkova <kenimaaa@gmail.com>
2022-06-03 21:25:42 +0300

    Finish second task

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,15 +1,14 @@
-// import Player from '@vimeo/player';
-var throttle = require('lodash.throttle');
 const iframe = document.querySelector('iframe');
 const player = new Vimeo.Player(iframe);
+var throttle = require('lodash.throttle');
 
-// player.on('play', function() {
-//     console.log('played the video!');
-// });
+player.on('play', function() {
+    console.log('played the video!');
+});
 
-// player.getVideoTitle().then(function(title) {
-//     console.log('title:', title);
-// });
+player.getVideoTitle().then(function(title) {
+    console.log('title:', title);
+});
 
 const onPlay = function(data) {
     console.log(data.seconds);
